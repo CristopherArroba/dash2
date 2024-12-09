c8061f4ee8ae82cfcb0914b01acc0ea75fa09e3e
CristopherArroba <86638941+CristopherArroba@users.noreply.github.com>
2024-12-09 10:52:25 -0500

    Delete dash directory

--- dash/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/dash2/dash",
-  plugins: [react()],
-})
